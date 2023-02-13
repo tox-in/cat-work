87a12b1041e8dcdc05d3947e0dafab04c0958ccf
tox-in <122396711+tox-in@users.noreply.github.com>
2023-02-13 16:58:07 +0200

    Add files via upload

---
+++ cat1.jsx
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express()
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.get('',(req,res)=>{
+    app.readFile(__dirname+"/cat1.ejs")
+});
+app.post("/",(req,res) =>{
+    const n1= req.body.num1
+     
+    const n2=req.body.num2 
+   
+    const multiply=n1*n2
+  res.send('the answer is'+multiply)
+})
+
+app.listen(8027,'localhost')
